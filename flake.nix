de51ea82e9de5d4989b9f89915aa048dede1f68e
Schahin Rouhanizadeh <schahin@rouhanizadeh.de>
2026-04-02 16:22:43 +0200

    Initial Commit!

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Basic Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+
+    createSpace = system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {};
+      };
+    in rec {
+      formatter = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            # Development tooling
+            git
+            tokei
+            ripgrep
+
+            # Web tooling
+            bun
+          ];
+        shellHook = ''
+          echo "Welcome to root dev shell on ${system}!"
+        '';
+      };
+    };
+  in {
+    devShells = forEachSystem (system: (createSpace system).devShells);
+    formatter = forEachSystem (system: (createSpace system).formatter);
+  };
+}
